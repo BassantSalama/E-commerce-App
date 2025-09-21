8fcf517509cbdcde9ef83f2c2a0ffdb7b44eeb13
Basant <basantsalama05@gmail.com>
2025-09-21 12:43:53 +0300

    add network layer

---
+++ ECommerceApp/Data/Network/APIRequest.swift
@@ -0,0 +1,38 @@
+//
+//  APIRequest.swift
+//  ECommerceApp
+//
+//  Created by mac on 21/09/2025.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET, POST, PUT, DELETE
+}
+
+enum ParameterEncoding {
+    case url
+    case json
+}
+
+struct APIRequest {
+    let url: URL
+    let method: HTTPMethod
+    let headers: [String: String]?
+    let parameters: [String: Any]?
+    let parameterEncoding: ParameterEncoding
+    
+    init(url: URL,
+         method: HTTPMethod = .GET,
+         headers: [String: String]? = nil,
+         parameters: [String: Any]? = nil,
+         parameterEncoding: ParameterEncoding = .url) {
+        self.url = url
+        self.method = method
+        self.headers = headers
+        self.parameters = parameters
+        self.parameterEncoding = parameterEncoding
+    }
+}
+
